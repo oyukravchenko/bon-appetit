5db715d5819378008b6e37bb78c57a25d83108e7
okravchenko <oleg.kravchenko@vsegda-da.com>
2020-10-06 21:59:21 +0300

    fix misprint in bon appetit

--- bonappetit/bonappetit/asgi.py
+++ bonappetit/bonappetit/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for bonappetite project.
+ASGI config for bonappetit project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bonappetite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bonappetit.settings')
 
 application = get_asgi_application()
